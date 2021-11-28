aecefc67d6e629cf721b9409ec02ea64792ff320
thanhvodev <thanhvo.dev@gmail.com>
2021-11-28 10:45:55 +0700

    fix button cart

--- Tan_Toan/cart.php
+++ Tan_Toan/cart.php
@@ -24,6 +24,7 @@ $order = mysqli_fetch_assoc($order);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -31,23 +32,23 @@ $order = mysqli_fetch_assoc($order);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel = "icon" href = "../IMAGE/logo.svg" type = "image/x-icon">
+    <link rel="icon" href="../IMAGE/logo.svg" type="image/x-icon">
     <link rel="stylesheet" href="../Huong/style/checkout.css">
     <title>Giỏ hàng của bạn</title>
     <script>
-            // delete product in cart
-        function delProInCart(ID){
+        // delete product in cart
+        function delProInCart(ID) {
             var xmlhttp = new XMLHttpRequest();
 
             xmlhttp.open("GET", "delProInCart.php?ProIDToDel=" + ID, false);
             xmlhttp.send();
             location.reload();
         }
-                // mode = 0 is decreasing; mode = 1 is increasing
-        function inOrDecreaseQty(PID,curQty, mode){
-            var newQty = (mode == 0)? curQty - 1 : curQty + 1;
+        // mode = 0 is decreasing; mode = 1 is increasing
+        function inOrDecreaseQty(PID, curQty, mode) {
+            var newQty = (mode == 0) ? curQty - 1 : curQty + 1;
             var xmlhttp = new XMLHttpRequest();
-            if(newQty == 0) delProInCart(PID);
+            if (newQty == 0) delProInCart(PID);
 
             xmlhttp.open("GET", "inOrDecreaseQty.php?newQty=" + newQty + "&PIDToChange=" + PID, false);
             xmlhttp.send();
@@ -55,6 +56,7 @@ $order = mysqli_fetch_assoc($order);
         }
     </script>
 </head>
+
 <body>
 
     <div class="row" style="padding-bottom:10px;background-color: #FFE4E1; min-height:60px;">
@@ -62,10 +64,10 @@ $order = mysqli_fetch_assoc($order);
             <img src="../IMAGE/logo.svg">
             <b style="align-self:center;margin-left:10px; font-size:20px;">Sokoshop</b>
         </div>
-        <div  class="col-sm-5" style="text-align: right;padding-top: 15px; padding-right:100px;">
-            <button onclick="window.location.href='../Tan_Toan/'">Trang chủ</button>
+        <div class="col-sm-5" style="text-align: right;padding-top: 15px; padding-right:100px;">
+            <button class="btn btn-primary" onclick="window.location.href='../Tan_Toan/'">Quay lại Trang chủ</button>
         </div>
-        </div>
+    </div>
 
     <div style="margin-top: 50px; text-align:center;">
         <b style="font-size:25px">Chi tiết giỏ hàng</b>
@@ -81,20 +83,21 @@ $order = mysqli_fetch_assoc($order);
                 <th class="bill-title" style="width:10%"> </th>
             </tr>
             <?php
-            if (mysqli_num_rows($cart) > 0) { 
+            if (mysqli_num_rows($cart) > 0) {
                 while ($row = mysqli_fetch_assoc($cart)) { ?>
-            <tr>
-                <td> &nbsp; <img src="<?php echo $row["IMG"] ?>" height="40px"> &nbsp;  &nbsp;<?php echo $row["NAME"] ?></td>
-                <td style="text-align: center;"><?php echo $row["PRICE"] ?></td>
-                <td style="text-align: center;">
-                    <button onclick = "inOrDecreaseQty(<?php echo '\''.$row['PID'].'\','. $row['QTY'];?>,0)" style="width:27px; margin-right:5px;">-</button>
-                    <?php echo $row["QTY"] ?>
-                    <button onclick = "inOrDecreaseQty(<?php echo '\''.$row['PID'].'\','. $row['QTY'];?>,1)" style="width:27px;margin-left:5px;">+</button>
-                </td>
-                <td style="text-align: center;"><?php echo $row["TOTAL"] ?></td>
-                <td style="text-align: center;"><button onclick="delProInCart('<?php echo $row['PID'];?>')" >Xóa</button></td>
-            </tr>
-            <?php }} ?>
+                    <tr>
+                        <td> &nbsp; <img src="<?php echo $row["IMG"] ?>" height="40px"> &nbsp; &nbsp;<?php echo $row["NAME"] ?></td>
+                        <td style="text-align: center;"><?php echo $row["PRICE"] ?></td>
+                        <td style="text-align: center;">
+                            <button class="btn btn-secondary" onclick="inOrDecreaseQty(<?php echo '\'' . $row['PID'] . '\',' . $row['QTY']; ?>,0)">-</button>
+                            <?php echo $row["QTY"] ?>
+                            <button class="btn btn-secondary" onclick="inOrDecreaseQty(<?php echo '\'' . $row['PID'] . '\',' . $row['QTY']; ?>,1)">+</button>
+                        </td>
+                        <td style="text-align: center;"><?php echo $row["TOTAL"] ?></td>
+                        <td style="text-align: center;"><button class="btn btn-danger" onclick="delProInCart('<?php echo $row['PID']; ?>')">Xóa</button></td>
+                    </tr>
+            <?php }
+            } ?>
         </table>
         <br>
         <b style="padding-left:33%; font-size:18px;">TỔNG CỘNG : </b>
@@ -104,22 +107,22 @@ $order = mysqli_fetch_assoc($order);
 
     <div class="foot-btn-container" style="min-height:200px; padding-top:100px">
         <button class="foot-btn" onclick="window.location.href='../Tan_Toan/'">Tiếp tục mua hàng</button>
-        <button class="foot-btn second-btn" onclick= "window.location.href='../Huong/Checkout-Vadidate.php'">Thanh toán</button>
+        <button class="foot-btn second-btn" onclick="window.location.href='../Huong/Checkout-Vadidate.php'">Thanh toán</button>
     </div>
 
-    <div style="text-align:center; background-color: #FFE4E1; padding-bottom:10px; margin-top: 50px;">   
+    <div style="text-align:center; background-color: #FFE4E1; padding-bottom:10px; margin-top: 50px;">
         <p>Copyright 2021 by Sokoshop. All Rights Reserved.</p>
         <img id="logo" src="../IMAGE/logo.svg" alt="Logo"> Sokoshop
     </div>
 
-<script>
-function my_function() {
-  var request = new XMLHttpRequest();
-  request.open("GET", "gotoAddressinfo.php", true);
-  request.send();
-  window.location.href='Checkout-Addressinfo.php';
-}
-</script>
+    <script>
+        function my_function() {
+            var request = new XMLHttpRequest();
+            request.open("GET", "gotoAddressinfo.php", true);
+            request.send();
+            window.location.href = 'Checkout-Addressinfo.php';
+        }
+    </script>
 
 </body>
 
--- Thanh/login.php
+++ Thanh/login.php
@@ -1,64 +1,63 @@
  <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["username"]) && !empty($_POST["password"])){
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST["username"]) && !empty($_POST["password"])) {
         require_once("../index.php");
-        if($_POST["password"] === "admin" && $_POST["username"] === "admin"){
+        if ($_POST["password"] === "admin" && $_POST["username"] === "admin") {
             $usr = "admin";
             $pwd = "admin";
             header("location: ./top-product.php");
-        }
-        else header("location: ../Tan_Toan/");
+        } else header("location: ../Tan_Toan/");
     }
- ?>
- 
+    ?>
+
  <!DOCTYPE html>
-<html lang="en">
+ <html lang="en">
 
-<head>
-    <title>How To Create Simple Login Form Design In Bootstrap 5</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://www.markuptag.com/bootstrap/5/css/bootstrap.min.css">
-</head>
+ <head>
+     <title>How To Create Simple Login Form Design In Bootstrap 5</title>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://www.markuptag.com/bootstrap/5/css/bootstrap.min.css">
+ </head>
 
-<body>
+ <body>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 offset-md-4">
-                <div class="login-form bg-light mt-4 p-4 shadow rounded">
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="row g-3">
-                        <h4>Quản lý đăng nhập</h4>
-                        <div class="col-12">
-                            <label>Tài khoản</label>
-                            <input type="text" name="username" class="form-control" placeholder="Tài khoản">
-                        </div>
-                        <div class="col-12">
-                            <label>Mật khẩu</label>
-                            <input type="password" name="password" class="form-control" placeholder="Mật khẩu">
-                        </div>
-                        <div class="col-12">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="rememberMe">
-                                <label class="form-check-label" for="rememberMe"> Ghi nhớ</label>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <!--a href="top-product.php" class="text-decoration-none text-white btn btn-dark float-end" role="button">Đăng nhập</a-->
-                            <button href="top-product.php" class="text-decoration-none text-white btn btn-dark float-end" role="button">Đăng nhập</button>
-                        </div>
-                    </form>
-                    <!-- <hr class="mt-4">
+     <div class="container">
+         <div class="row">
+             <div class="col-md-4 offset-md-4">
+                 <div class="login-form bg-light mt-4 p-4 shadow rounded">
+                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="row g-3">
+                         <h4>Quản lý đăng nhập</h4>
+                         <div class="col-12">
+                             <label>Tài khoản</label>
+                             <input type="text" name="username" class="form-control" placeholder="Tài khoản">
+                         </div>
+                         <div class="col-12">
+                             <label>Mật khẩu</label>
+                             <input type="password" name="password" class="form-control" placeholder="Mật khẩu">
+                         </div>
+                         <div class="col-12">
+                             <div class="form-check">
+                                 <input class="form-check-input" type="checkbox" id="rememberMe">
+                                 <label class="form-check-label" for="rememberMe"> Ghi nhớ</label>
+                             </div>
+                         </div>
+                         <div class="col-12">
+                             <!--a href="top-product.php" class="text-decoration-none text-white btn btn-dark float-end" role="button">Đăng nhập</a-->
+                             <button href="top-product.php" class="text-decoration-none text-white btn btn-dark float-end" role="button">Đăng nhập</button>
+                         </div>
+                     </form>
+                     <!-- <hr class="mt-4">
                     <div class="col-12">
                         <p class="text-center mb-0">Have not account yet? <a href="#">Signup</a></p>
                     </div> -->
-                </div>
-            </div>
-        </div>
-    </div>
+                 </div>
+             </div>
+         </div>
+     </div>
 
-    <!-- Bootstrap JS -->
-    <script src="https://www.markuptag.com/bootstrap/5/js/bootstrap.bundle.min.js"></script>
-</body>
+     <!-- Bootstrap JS -->
+     <script src="https://www.markuptag.com/bootstrap/5/js/bootstrap.bundle.min.js"></script>
+ </body>
 
-</html>
+ </html>
